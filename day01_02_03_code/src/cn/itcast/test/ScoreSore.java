32988c8cedd6a2466a28464d662924f6f56e76c0
wmanbs <wmanbs@outlook.com>
2026-01-06 19:45:25 +0800

    全部回来了

--- day01_02_03_code/src/cn/itcast/test/ScoreSore.java
+++ day01_02_03_code/src/cn/itcast/test/ScoreSore.java
@@ -2,6 +2,7 @@ package cn.itcast.test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ScoreSore {
@@ -12,6 +13,8 @@ public class ScoreSore {
         int n = sc.nextInt();
         String[] names = new String[n];
         int[] scores = new int[n];
+        String[] namesScores = new String[n];
+        String[] temp0 = new String[2];
 
         for (int i = 0; i < n; i++) {
             names[i] = sc.next();
@@ -34,7 +37,23 @@ public class ScoreSore {
         }
 
         for (int i = 0; i < n; i++) {
-            System.out.println(names[i]+" "+scores[i]);
+            namesScores[i] = (names[i] + " " + scores[i]);
+        }
+
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < n - 1; i++) {
+                if (scores[i] == scores[i + 1]) {
+                    temp0[0] = namesScores[i];
+                    temp0[1] = namesScores[i + 1];
+                    Arrays.sort(temp0, String.CASE_INSENSITIVE_ORDER);
+                    namesScores[i] = temp0[0];
+                    namesScores[i + 1] = temp0[1];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(namesScores[i]);
         }
 
     }
