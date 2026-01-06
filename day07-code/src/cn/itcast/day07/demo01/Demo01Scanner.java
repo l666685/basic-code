f31b0ed7243fe3615eeb733a5a2d1d1d31204f02
wmanbs <wmanbs@outlook.com>
2026-01-06 17:13:22 +0800

    全部回来了

--- day07-code/src/cn/itcast/day07/demo01/Demo01Scanner.java
+++ day07-code/src/cn/itcast/day07/demo01/Demo01Scanner.java
@@ -1,8 +1,31 @@
 package cn.itcast.day07.demo01;
+
+import java.util.Scanner; //1. 导包
+
 /*
 Scanner类的功能：可以实现键盘输入数据，到程序中。
 
+引用类型的一般使用步骤：
 
+1. 导包
+import 包名称.类名称;
+对于和当前类属于同一个包的情况，可以省略导包语句不写。
+只有java.lang包下的内容不需要导包，其他的包都需要import语句。
+2. 创建
+类名称.对象名 = new 类名称();
+
+3. 使用
+对象名.成员方法名()
  */
 public class Demo01Scanner {
+
+    public static void main(String[] args) {
+
+        //2. 创建
+        //System.in代表从键盘输入
+        //其他的写法自己查JDK API去
+        Scanner sc = new Scanner(System.in);
+
+    }
+
 }
